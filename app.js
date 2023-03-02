edd6d5ba5ad4cdd6219e1d356e5457fb3695a815
Pawan <pawans6343@gmail.com>
2023-03-03 03:21:15 +0530

    g

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ app.use(cors());
 dotenv.config({ path: "./.env" });
 
 require("./models/database").dbConnection();
-
+app.use(function (req, res, next) {
+  //Enabling CORS
+ 
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
