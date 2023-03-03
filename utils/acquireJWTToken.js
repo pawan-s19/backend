ce0a1ce9d27a5cec0d9434c1bc16db27867ceeb9
Pawan <pawans6343@gmail.com>
2023-03-03 14:22:49 +0530

    clg for token

--- utils/acquireJWTToken.js
+++ utils/acquireJWTToken.js
@@ -1,7 +1,7 @@
 //creating token and saving it in coookie
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
-
+  console.log(token);
   //cookie options
   const options = {
     expires: new Date(Date.now() + 1 * 60 * 24 * 24 * 1000),
